659339ef9d10be99f6fdd87ce47f91492d194b67
ermondel <ermondel@gmail.com>
2019-02-25 11:23:36 +0200

    exercise: subscribing and dynamic actions

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -3,9 +3,13 @@ import { createStore } from 'redux';
 const store = createStore((state = { count: 0 }, action) => {
     switch (action.type) {
         case 'INCREMENT':
-            return { count: state.count + 1 };
+            const i = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
+            return { count: state.count + i };
         case 'DECREMENT':
-            return { count: state.count - 1 };
+            const d = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
+            return { count: state.count - d };
+        case 'SET':
+            return { count: action.count }
         case 'RESET':
             return { count: 0 };
         default:
@@ -13,29 +17,25 @@ const store = createStore((state = { count: 0 }, action) => {
     }
 });
 
+const unsubscribe = store.subscribe(() => {
+    console.log(store.getState());
+});
 
-console.log(store.getState());
+store.dispatch({ 
+    type: 'INCREMENT',
+    incrementBy: 5
+});
 
-store.dispatch({ type: 'INCREMENT' });
-store.dispatch({ type: 'INCREMENT' });
-store.dispatch({ type: 'INCREMENT' });
+store.dispatch({ 
+    type: 'DECREMENT',
+});
 
-console.log(store.getState());
+store.dispatch({ 
+    type: 'DECREMENT',
+    decrementBy: 10
+});
 
-store.dispatch({ type: 'DECREMENT' });
-store.dispatch({ type: 'DECREMENT' });
-store.dispatch({ type: 'DECREMENT' });
-
-console.log(store.getState());
-
-store.dispatch({ type: 'DECREMENT' });
-store.dispatch({ type: 'DECREMENT' });
-store.dispatch({ type: 'DECREMENT' });
-store.dispatch({ type: 'DECREMENT' });
-store.dispatch({ type: 'DECREMENT' });
-
-console.log(store.getState());
-
-store.dispatch({ type: 'RESET' });
-
-console.log(store.getState());
+store.dispatch({
+    type: 'SET',
+    count: 101
+});

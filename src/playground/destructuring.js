5851d19c8b76e574ab85992525f8322b003ad672
ermondel <ermondel@gmail.com>
2019-02-27 16:12:43 +0200

    exercise: es6 array destructuring

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,3 +1,7 @@
+//
+// Object destructuring
+//
+
 // const person = {
 //     name: 'Archimedes',
 //     age: 64,
@@ -15,14 +19,26 @@
 //     console.log(`It's ${temperature} in ${city}`);
 // }
 
-const book = {
-    title: 'Ego is the Enemy',
-    author: 'Ryan Holiday',
-    publisher: {
-        name: 'Penguin'
-    }
-}
+// const book = {
+//     title: 'Ego is the Enemy',
+//     author: 'Ryan Holiday',
+//     publisher: {
+//         name: 'Penguin'
+//     }
+// }
 
-const { name: publisherName = 'Self-Published' } = book.publisher;
+// const { name: publisherName = 'Self-Published' } = book.publisher;
 
-console.log(publisherName);
+// console.log(publisherName);
+
+//
+// Array destructuring
+//
+
+// const address = [ '1299 S Juniper Street', 'Philadelphia', 'Pennsylvania', '19147' ];
+// const [ , city, state = 'New York' ] = address;
+// console.log(`You are in ${city} ${state}.`);
+
+const item = [ 'Coffee (iced)', '$3.00', '$4.70', '$5.95' ];
+const [ itemName, , mediumPrice ] = item;
+console.log(`A medium ${itemName} costs ${mediumPrice}`);

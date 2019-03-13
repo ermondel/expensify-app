43f7cdd1d7a1fbced4c4db54b669af63a39f978d
ermondel <ermondel@gmail.com>
2019-03-13 21:17:57 +0200

    exercise: spreading objects

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -24,6 +24,19 @@ const removeExpense = ({ id } = {}) => ({
     id
 });
 
+// EDIT_EXPENSE
+const editExpense = (id, updates) => ({
+    type: 'EDIT_EXPENSE',
+    id,
+    updates
+});
+
+// SET_TEXT_FILTER
+const setTextFilter = (text = '') => ({
+    type: 'SET_TEXT_FILTER',
+    text
+});
+
 // Expenses Reducer
 const expensesReducerDefaultState = [];
 
@@ -33,6 +46,17 @@ const expensesReducer = (state = expensesReducerDefaultState, action) => {
             return [...state, action.expense];
         case 'REMOVE_EXPENSE':
             return state.filter(({ id }) => id !== action.id);
+        case 'EDIT_EXPENSE':
+            return state.map(expense => {
+                if (expense.id === action.id) {
+                    return {
+                        ...expense,
+                        ...action.updates
+                    };
+                } else {
+                    return expense;
+                }
+            });
         default:
             return state;
     }
@@ -40,7 +64,7 @@ const expensesReducer = (state = expensesReducerDefaultState, action) => {
 
 // Filters Reducer
 const filtersReducerDefaultState = {
-    text: 'rent',
+    text: '',
     sortBy: 'date',
     startDate: undefined,
     endDate: undefined
@@ -48,6 +72,11 @@ const filtersReducerDefaultState = {
 
 const filtersReducer = (state = filtersReducerDefaultState, action) => {
     switch (action.type) {
+        case 'SET_TEXT_FILTER':
+            return { 
+                ...state, 
+                text: action.text 
+            };
         default:
             return state;
     }
@@ -69,20 +98,35 @@ const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 30
 
 store.dispatch(removeExpense({ id: expenseOne.expense.id }));
 
+store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
+
+store.dispatch(setTextFilter('rent'));
+store.dispatch(setTextFilter());
+
 // demo state
 
-const demoState = {
-    expenses: [{
-        id: 'f84jg78hfy57',
-        description: 'January Rent',
-        note: 'This was the final payment for that address',
-        amount: 54500,
-        createdAt: 0
-    }],
-    filters: {
-        text: 'rent',
-        sortBy: 'amount',
-        startDate: undefined,
-        endDate: undefined
-    }
-}
+// const demoState = {
+//     expenses: [{
+//         id: 'f84jg78hfy57',
+//         description: 'January Rent',
+//         note: 'This was the final payment for that address',
+//         amount: 54500,
+//         createdAt: 0
+//     }],
+//     filters: {
+//         text: 'rent',
+//         sortBy: 'amount',
+//         startDate: undefined,
+//         endDate: undefined
+//     }
+// }
+
+// const user = {
+//     name: 'Jen',
+//     age: 24
+// }
+
+// console.log({
+//     ...user,
+//     location: 'Kiev'
+// });

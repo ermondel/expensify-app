7d173496e4ced2c6ba9b34dfc0c916692b66b251
ermondel <ermondel@gmail.com>
2019-03-15 13:03:37 +0200

    exercise: filtering redux data

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -6,7 +6,7 @@ const addExpense = ({
     description = '',
     note = '',
     amount = 0,
-    createAt = 0
+    createdAt = 0
 } = {}) => ({
     type: 'ADD_EXPENSE',
     expense: {
@@ -14,7 +14,7 @@ const addExpense = ({
         description,
         note,
         amount,
-        createAt
+        createdAt
     }
 });
 
@@ -124,6 +124,16 @@ const filtersReducer = (state = filtersReducerDefaultState, action) => {
     }
 }
 
+// Get visible expenses
+const getVisibleExpenses = (expense, { text, sortBy, startDate, endDate }) => {
+    return expense.filter(expense => {
+        const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
+        const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate;
+        const textMatch = text.length && expense.description.toLowerCase().includes(text.toLowerCase());
+        return startDateMatch && endDateMatch && textMatch;
+    });
+}
+
 // Store creation
 
 const store = createStore(combineReducers({
@@ -132,26 +142,28 @@ const store = createStore(combineReducers({
 }));
 
 store.subscribe(() => {
-    console.log(store.getState());
+    const state = store.getState();
+    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+    console.log(visibleExpenses);
 });
 
-// const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100 }));
-// const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 300 }));
+const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100, createdAt: 1000 }));
+const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 300, createdAt: -1000 }));
 
 // store.dispatch(removeExpense({ id: expenseOne.expense.id }));
 
 // store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
 
 // store.dispatch(setTextFilter('rent'));
-// store.dispatch(setTextFilter());
+// store.dispatch(setTextFilter('e'));
 
 // store.dispatch(sortByAmount());
 // store.dispatch(sortByDate());
 
-store.dispatch(setStartDate(125));
-store.dispatch(setStartDate());
-store.dispatch(setEndDate(1250));
-store.dispatch(setEndDate());
+// store.dispatch(setStartDate(0));
+// store.dispatch(setStartDate());
+// store.dispatch(setEndDate(999));
+// store.dispatch(setEndDate());
 
 // demo state
 

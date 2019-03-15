fd1af89a5a1bd1c1f5f4ce8c3f70ec9d21162661
ermondel <ermondel@gmail.com>
2019-03-15 14:41:33 +0200

    exercise: sorting redux data

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -125,12 +125,18 @@ const filtersReducer = (state = filtersReducerDefaultState, action) => {
 }
 
 // Get visible expenses
-const getVisibleExpenses = (expense, { text, sortBy, startDate, endDate }) => {
-    return expense.filter(expense => {
+const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
+    return expenses.filter(expense => {
         const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
         const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate;
-        const textMatch = text.length && expense.description.toLowerCase().includes(text.toLowerCase());
+        const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
         return startDateMatch && endDateMatch && textMatch;
+    }).sort((a, b) => {
+        if (sortBy === 'date') {
+            return a.createdAt < b.createdAt ? 1 : -1;
+        } else if (sortBy === 'amount') {
+            return a.amount < b.amount ? 1 : -1;
+        }
     });
 }
 
@@ -147,7 +153,7 @@ store.subscribe(() => {
     console.log(visibleExpenses);
 });
 
-const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100, createdAt: 1000 }));
+const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100, createdAt: -1200 }));
 const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 300, createdAt: -1000 }));
 
 // store.dispatch(removeExpense({ id: expenseOne.expense.id }));
@@ -157,7 +163,7 @@ const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 30
 // store.dispatch(setTextFilter('rent'));
 // store.dispatch(setTextFilter('e'));
 
-// store.dispatch(sortByAmount());
+store.dispatch(sortByAmount());
 // store.dispatch(sortByDate());
 
 // store.dispatch(setStartDate(0));

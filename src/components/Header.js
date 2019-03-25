788a343d1a91986e2d3ea0b1b4d7f226fa9eb7cc
ermondel <ermondel@gmail.com>
2019-03-25 10:44:06 +0200

    feat: display main navigation menu as a list

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,12 @@ import { NavLink } from 'react-router-dom';
 const Header = () => (
     <header>
         <h1>Expensify</h1>
-        <NavLink to="/" activeClassName="is-active" exact>Dashboard</NavLink> 
-        <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink> 
-        <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink> 
-        <NavLink to="/help" activeClassName="is-active">Help</NavLink>
+        <ul>
+            <li><NavLink to="/" activeClassName="is-active" exact>Dashboard</NavLink></li>
+            <li><NavLink to="/create" activeClassName="is-active">Create Expense</NavLink></li>
+            <li><NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink></li>
+            <li><NavLink to="/help" activeClassName="is-active">Help</NavLink></li>
+        </ul>
     </header>
 );
 

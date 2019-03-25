ac2ea7f96f8db6cd74714124614262062cc9db40
ermondel <ermondel@gmail.com>
2019-03-25 12:35:40 +0200

    exercise: controlled inputs for filters

---
+++ src/components/ExpenseListFilters.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { setTextFilter } from '../actions/filters';
+
+const ExpenseListFilters = props => (
+    <div>
+        <input type="text" value={props.filters.text} onChange={event => {
+            props.dispatch(setTextFilter(event.target.value));
+        }} />
+    </div>
+);
+
+const mapStateToProps = state => ({
+    filters: state.filters
+});
+
+export default connect(mapStateToProps)(ExpenseListFilters);

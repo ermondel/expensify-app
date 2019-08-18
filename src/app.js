02f51b6c531e6a74141bc49a4afaa8cd66e9fe46
Ermondel <ermondel@gmail.com>
2019-08-18 13:19:53 +0300

    chore: delete debug info

--- src/app.js
+++ src/app.js
@@ -3,26 +3,16 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill', amount: 4500, createdAt: -38156400000 }));
-store.dispatch(addExpense({ description: 'Gas bill', amount: 5600, createdAt: 1000000387563 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 109500, createdAt: 1010100958764 }));
-
-const state = store.getState();
-console.log(getVisibleExpenses(state.expenses, state.filters));
-
 const jsx = (
-    <Provider store={store}>
-        <AppRouter />
-    </Provider>
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
 );
 
 ReactDOM.render(jsx, document.getElementById('app'));

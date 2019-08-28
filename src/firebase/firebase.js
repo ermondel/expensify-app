4d07618f0f874b3d7cd606ec253bcb3a9217d985
Ermondel <ermondel@gmail.com>
2019-08-28 15:35:33 +0300

    writing to the database

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,9 +4,25 @@ const firebaseConfig = {};
 
 firebase.initializeApp(firebaseConfig);
 
-firebase
-  .database()
-  .ref()
-  .set({
-    name: 'erm'
-  });
+const database = firebase.database();
+
+database.ref().set({
+  name: 'erm',
+  age: 1,
+  isSingle: true,
+  location: {
+    city: 'San Francisco',
+    country: 'United States'
+  }
+});
+
+// database.ref().set('This is my data.');
+
+database.ref('age').set(4);
+database.ref('location/city').set('San Jose');
+database.ref('attributes').set({
+  height: 2,
+  weight: 2
+});
+
+console.log('database ok');

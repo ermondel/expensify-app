5be98db7490af4261d91f1bce1a2088614b175aa
Ermondel <ermondel@gmail.com>
2019-08-28 22:12:01 +0300

    fetching data from firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -6,33 +6,81 @@ firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
 
-database
-  .ref()
-  .set({
-    name: 'erm',
-    age: 1,
-    stressLevel: 6,
-    job: {
-      title: 'Software developer',
-      company: 'Google'
-    },
-    location: {
-      city: 'San Francisco',
-      country: 'United States'
-    }
-  })
-  .then(() => {
-    console.log('Data is saved');
-  })
-  .catch(error => {
-    console.log('This failed.', error);
-  });
+database.ref().on(
+  'value',
+  snapshot => {
+    const val = snapshot.val();
+    console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
+  },
+  error => {
+    console.log('Error with data fetching.', error);
+  }
+);
 
-database.ref().update({
-  stressLevel: 9,
-  'job/company': 'Amazon',
-  'location/city': 'Seattle'
-});
+setTimeout(() => {
+  database.ref('name').set('Mike');
+}, 2500);
+
+// const onValueChange = database.ref().on(
+//   'value',
+//   snapshot => {
+//     console.log(snapshot.val());
+//   },
+//   error => {
+//     console.log('Error with data fetching.', error);
+//   }
+// );
+
+// setTimeout(() => {
+//   database.ref('age').set(1);
+// }, 3500);
+
+// setTimeout(() => {
+//   database.ref().off('value', onValueChange);
+// }, 7000);
+
+// setTimeout(() => {
+//   database.ref('age').set(3);
+// }, 10500);
+
+// database
+//   .ref()
+//   .once('value')
+//   .then(snapshot => {
+//     const val = snapshot.val();
+//     console.log(val);
+//   })
+//   .catch(error => {
+//     console.log('Error fetching data', error);
+//   });
+
+// database
+//   .ref()
+//   .set({
+//     name: 'erm',
+//     age: 1,
+//     stressLevel: 6,
+//     job: {
+//       title: 'Software developer',
+//       company: 'Google'
+//     },
+//     location: {
+//       city: 'San Francisco',
+//       country: 'United States'
+//     }
+//   })
+//   .then(() => {
+//     console.log('Data is saved');
+//   })
+//   .catch(error => {
+//     console.log('This failed.', error);
+//   });
+
+// database.ref().update({
+//   stressLevel: 9,
+//   'job/company': 'Amazon',
+//   'location/city': 'Seattle'
+// });
 
 // database.ref().update({
 //   job: 'Manager',

84ee3eb488c9bce4e28b2d0575b13563f9531ff3
Ermondel <ermondel@gmail.com>
2019-08-28 17:40:59 +0300

    es6 promises

---
+++ src/playground/promises.js
@@ -0,0 +1,21 @@
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    // resolve({
+    //   file: 'readme',
+    //   time: '1567002450'
+    // });
+    reject('Error message');
+  }, 1500);
+});
+
+console.log('before');
+
+promise
+  .then(data => {
+    console.log('1', data);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+console.log('after');

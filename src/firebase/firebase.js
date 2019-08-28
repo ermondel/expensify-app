85298c0d2d8609452530a1e0df2e580f10ec7fed
Ermondel <ermondel@gmail.com>
2019-08-28 19:17:51 +0300

    removing data from firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -24,18 +24,30 @@ database
     console.log('This failed.', error);
   });
 
+database.ref('isSingle').set(null);
+
 // database.ref('age').set(4);
 // database.ref('location/city').set('San Jose');
 
-database
-  .ref('attributes')
-  .set({
-    height: 3,
-    weight: 3
-  })
-  .then(() => {
-    console.log('Data is saved');
-  })
-  .catch(error => {
-    console.log('This failed.', error);
-  });
+// database
+//   .ref('attributes')
+//   .set({
+//     height: 3,
+//     weight: 3
+//   })
+//   .then(() => {
+//     console.log('Data is saved');
+//   })
+//   .catch(error => {
+//     console.log('This failed.', error);
+//   });
+
+// database
+//   .ref()
+//   .remove()
+//   .then(() => {
+//     console.log('Data was removed');
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   });
